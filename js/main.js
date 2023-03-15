4faa03701d219dd1e0edc2b355b2b39598cf9446
Mihail Nechaev <sadovni4ek@gmail.com>
2023-03-15 14:07:59 +0300

    adaptive added

--- js/main.js
+++ js/main.js
@@ -1,43 +1,30 @@
-/*const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabItem = document.querySelectorAll(".tabs__btn-item");
 const tabContent = document.querySelectorAll(".tabs__content-item");
 
-tabItem.forEach((el) => {
-    el.addEventListener("click", () => {
-        let currentBtn = el;
-        let tabId = currentBtn.getAttribute("data-button");
-        let currentTab = document.querySelector(tabId);
-        
-        if (!currentBtn.classList.contains("tabs__btn-item--active")) {
-            tabItem.forEach((el) => {
-                el.classList.remove("tabs__btn-item--active");
-            });
-            tabContent.forEach((el) => {
-                el.classList.remove("tabs__btn-item--active");
-            });
-               
-            currentBtn.classList.add("tabs__btn-item--active");
-            currentTab.classList.add("tabs__btn-item--active");
-            }
-        })
-    })*/
+tabItem.forEach(function (element) {
+  element.addEventListener("click", switcher);
+});
+function switcher(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
 
-    const tabItem = document.querySelectorAll(".tabs__btn-item");
-    const tabContent = document.querySelectorAll(".tabs__content-item");
+  tabItem.forEach(function (item) {
+    item.classList.remove("tabs__btn-item--active");
+  });
+  tabContent.forEach(function (item) {
+    item.classList.remove("tabs__content-item--active");
+  });
 
-    tabItem.forEach(function(element) {
-      element.addEventListener("click", switcher);
-    });
-    function switcher(evt) {
-        const tabTarget = evt.currentTarget;
-        const button = tabTarget.dataset.button;
+  tabTarget.classList.add("tabs__btn-item--active");
+  document
+    .querySelector(`#${button}`)
+    .classList.add("tabs__content-item--active");
+}
 
-        tabItem.forEach(function(item) {
-            item.classList.remove("tabs__btn-item--active");
-        });
-        tabContent.forEach(function(item) {
-            item.classList.remove("tabs__content-item--active");
-        });
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
 
-        tabTarget.classList.add("tabs__btn-item--active");
-        document.querySelector(`#${button}`).classList.add("tabs__content-item--active")
-    }
+menuBtn.addEventListener("click", () => {
+    
+    menu.classList.toggle("menu--active")
+})
